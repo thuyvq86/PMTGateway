9b6dcf95108c82995a70ccd9ec9818a4d8b6558d
thanhdt <thanhdt9279@yahoo.com>
2013-12-06 18:45:51 +0700

    Create CacheForm.php
    add

---
+++ src/protected/models/CacheForm.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * ReportForm class.
+ * ReportForm is the data structure for keeping
+ * report form data. It is used by the 'report' action of 'MerchantController'.
+ */
+class CacheForm
+{
+	public $firstname;
+	public $lastname;                
+	public $company;
+	public $address1;
+	public $address2;
+	public $city;
+	public $country;
+	public $zipcode;
+	public $state;
+	public $email;
+	public $phone;     
+	public $refnumber;
+	public $amount;
+	public $currency;
+}
