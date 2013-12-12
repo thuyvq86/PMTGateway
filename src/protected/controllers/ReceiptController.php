62a41dc1b680b8a07864e001f895828036c9435b
thanhdt <thanhdt9279@yahoo.com>
2013-12-12 23:41:02 +0700

    edit receipt controller
    ok

--- src/protected/controllers/ReceiptController.php
+++ src/protected/controllers/ReceiptController.php
@@ -19,21 +19,27 @@ class ReceiptController extends Controller
 		
 	
 	public function actionRecept()
-	{					
+	{							
 		$response = "";		
 		foreach($_REQUEST as $name => $value) 
-		{
-				 $params[$name] = $value;                     
-				 $output = "<input  type=\"text\" name=\"" . $name . "\" size=\"50\" value=\"" . $value . "\" readonly=\"true\"/>\n";
-				$response .= $output;
+		{			
+			 $params[$name] = $value;                     
+			 $output = "<input  type=\"text\" name=\"" . $name . "\" size=\"50\" value=\"" . $value . "\" readonly=\"true\"/>\n";
+			$response .= $output;
 		}
-									 
+		if(!isset($params["req_reference_number"])){
+			$err = "Response Error";
+			$url = $this -> createUrl("BillPayment/error", array('ErrMsg'=>$err));			
+			$this -> redirect($url);
+			return;
+		}else{
+			$res_refnumber = $params["req_reference_number"];		
+		}
+	
 		$connection = Yii::app() -> db;
 		$connection -> active = true;
 	
-		$sql = "UPDATE cp_requestlog SET RespCode = :RespCode, ResponseBody = :ResponseBody WHERE ReferenceNumber = :ReferenceNumber";						
-		$res_refnumber = $params["req_reference_number"];		
-		echo $res_refnumber;
+		$sql = "UPDATE cp_requestlog SET RespCode = :RespCode, ResponseBody = :ResponseBody WHERE ReferenceNumber = :ReferenceNumber";										
 		
 		$respbody = $response;
 		$respcode = "00";
